ae5ccc64d2be2cbafa384389e69ef4511a791772
Yeswanthrepaka <yeswanthrepaka@gmail.com>
2026-02-02 04:16:52 +0530

    Changes done

--- 02-variables.sh
+++ 02-variables.sh
@@ -2,9 +2,9 @@
 
 NUMBER=$1
 
-if [ $NUMBER -gt 20]; then
+if [ $NUMBER -gt 20 ]; then
     echo "Given number is greater than 20"
-elif [ $NUMBER -et 20]; then
+elif [ $NUMBER -et 20 ]; then
     echo "Given number is equal to 20"
 else
     echo "Given number is less than 20"
