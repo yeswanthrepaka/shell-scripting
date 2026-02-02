b022e6e78ba6ccb5be248c9b83f035bcd2a9d1fe
Yeswanthrepaka <yeswanthrepaka@gmail.com>
2026-02-03 03:55:13 +0530

    Changes done

---
+++ 06-loops.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run the script with sudo user"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2... FAILURE"
+    else
+        echo "$2... SUCCESS"
+    fi
+}
+
+for package in $@
+    do
+        if [ $? -ne 0 ]; then
+            echo "$package not installed... Now installing"
+            dnf install $package -y
+            VALIDATE $? "Installing $package"
+        else
+            echo "$package already installed... SKIPPING"
+        fi
+    done
