fbfe6424bfa2a8de4366a57af01d70b95d67595a
Yeswanthrepaka <yeswanthrepaka@gmail.com>
2026-02-03 04:10:20 +0530

    Changes done

---
+++ 07-colours.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo "$R Please run the script with sudo user $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2... $R FAILURE $N"
+    else
+        echo "$2... $G SUCCESS $N"
+    fi
+}
+
+for package in $@
+    do
+        dnf list installed $package
+        if [ $? -ne 0 ]; then
+            echo "$R $package not installed... Now installing $N"
+            dnf install $package -y
+            VALIDATE $? "Installing $package"
+        else
+            echo "$Y $package already installed... SKIPPING $N"
+        fi
+    done
