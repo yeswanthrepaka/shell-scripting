78fea2af0268d76db6e1c2b739186220d108526b
Yeswanthrepaka <yeswanthrepaka@gmail.com>
2026-02-03 04:25:47 +0530

    Changes done

--- 08-logs.sh
+++ 08-logs.sh
@@ -14,15 +14,15 @@ N="\e[0m"
 mkdir -p $LOGS_FOLDER
 
 if [ $USERID -ne 0 ]; then
-    echo -e "$R Please run the script with sudo user $N"
+    echo -e "$R Please run the script with sudo user $N" | tee -a $LOGS_FILE
     exit 1
 fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo -e "$2... $R FAILURE $N"
+        echo -e "$2... $R FAILURE $N" | tee -a $LOGS_FILE
     else
-        echo -e "$2... $G SUCCESS $N"
+        echo -e "$2... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi
 }
 
