dcae2da0d76fa312f463e23893647b13e0abed3e
Yeswanthrepaka <yeswanthrepaka@gmail.com>
2026-02-03 04:32:25 +0530

    Changes done

--- 08-logs.sh
+++ 08-logs.sh
@@ -21,6 +21,7 @@ fi
 VALIDATE(){
     if [ $1 -ne 0 ]; then
         echo -e "$2... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
     else
         echo -e "$2... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi
