69fbf959c625fbb14bfd16dda767709fd90c06fb
yeswanthrepaka <yeswanthrepaka@gmail.com>
2026-02-11 12:51:32 +0530

    changes done

--- 09-while.sh
+++ 09-while.sh
@@ -4,7 +4,10 @@ count=1
 
 while [ $count -lt 5 ];
 do
-    echo "Number $count"
+    echo "Number $count" 
+    # Print number less than 5
     sleep 1
+    # Sleep for 1 second
     ((count++))
+    # Increase the count by one number
 done
--- 10-while.sh
+++ 10-while.sh
@@ -3,4 +3,5 @@
 while IFS= read -r line; 
 do
     echo "$line"
+    # Read and print every line of the below file
 done < 09-while.sh
