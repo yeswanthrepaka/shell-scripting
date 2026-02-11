ee3f4546cd266d14f1be6e2c815e03fd08d9aa95
yeswanthrepaka <yeswanthrepaka@gmail.com>
2026-02-11 19:05:07 +0530

    changes done

---
+++ 12-backup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/shell-scrips"
+LOGS_FILE="/var/log/shell-scrips/backup.log"
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=$(3:-14) #Default 14 days if not given
+USERID=$(id -u)
+
+usage(){
+    echo "Minimum two args <source> <dest> <days- if not given it will take default 14>"
+}
+
+if [ $# -lt 2 ]; then
+    usage
+    exit 1
+fi
+
+if [ ! -d $SOURCE_DIR ]; then
+    echo "$SOURCE_DIR not available"
+    exit 1
+fi
+
+if [ ! -d $DEST_DIR ]; then
+    echo "$DEST_DIR not available"
+    exit 1
+fi
+
+TIME=$(date)
+FILE_FIND=$(find $SOURCE_DIR -type f -mtime +$DAYS )
+ZIP_FILE_NAME="backup-$TIME.tar.gz"
+
+if [ -z $SOURCE_DIR ]; then
+    echo "$SOURCE_DIR is empty"
+else
+    tar -zcvf $ZIP_FILE_NAME $FILE_FIND
+    echo "ZIP process comleted"
+fi
+
